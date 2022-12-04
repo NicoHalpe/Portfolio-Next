cb5f11f7c5ab83afcbb564493f6005c5f3e470d4
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2022-12-04 16:14:42 -0300

    Added Zerti and improved carrousel

--- components/SectionProyects.jsx
+++ components/SectionProyects.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Image from "next/image";
 import React, { useEffect, useState, useRef } from "react";
+import { useSwipeable } from "react-swipeable";
 import useIntersectionObserver from "./../hooks/useIntersectionObserver";
 
 import styles from "./SectionProyects.module.css";
@@ -8,11 +9,94 @@ import styles from "./SectionProyects.module.css";
 export default function SectionProyects() {
 	const [originals, setOriginals] = useState([]);
 	const [images, setImages] = useState();
-	const startCard = 3;
+	const [startCard, setStartCard] = useState(1);
 	const [cardWidth, setCardWidth] = useState();
 	const [mobileDif, setMobileDif] = useState(0);
 	const ref = useRef();
 	const onScreen = useIntersectionObserver(ref, { rootMargin: "-150px" });
+
+	const handleCardClickPos = (dif) => {
+		const check = images.querySelectorAll(`.${styles.card}`)[
+			images.querySelectorAll(`.${styles.card}`).length - 1
+		];
+		let currentOriginalPos =
+			originals.findIndex(
+				(original) =>
+					original.querySelector(`.${styles["card-title"]}`).innerHTML ===
+					check.querySelector(`.${styles["card-title"]}`).innerHTML
+			) + 1;
+		if (currentOriginalPos > originals.length - 1) {
+			currentOriginalPos = currentOriginalPos - originals.length;
+		}
+		let currentCard = startCard;
+		images.style.setProperty("pointer-events", "none");
+		currentCard += dif;
+		images.style.transitionDuration = "0.5s";
+		images.style.transform = `translate(-${currentCard * cardWidth - mobileDif}px)`;
+		for (var i = 0; i < dif; i++) {
+			const clone = originals[currentOriginalPos].cloneNode(true);
+			images.appendChild(clone);
+			currentOriginalPos += 1;
+			if (currentOriginalPos > originals.length - 1) {
+				currentOriginalPos = currentOriginalPos - originals.length;
+			}
+		}
+		images.children[currentCard].classList.add(styles.visible);
+		images.children[currentCard - dif].classList.remove(styles.visible);
+
+		setTimeout(() => {
+			images.style.setProperty("pointer-events", "all");
+			currentCard -= dif;
+			for (var i = 0; i < dif; i++) {
+				images.children[0].remove();
+			}
+			images.style.transitionDuration = "0s";
+			images.style.transform = `translate(-${currentCard * cardWidth - mobileDif}px)`;
+		}, 500);
+	};
+
+	const handleCardClickNeg = (dif) => {
+		let currentCard = startCard;
+		const check = images.querySelectorAll(`.${styles.card}`)[0];
+		let currentOriginalNeg =
+			originals.findIndex(
+				(original) =>
+					original.querySelector(`.${styles["card-title"]}`).innerHTML ===
+					check.querySelector(`.${styles["card-title"]}`).innerHTML
+			) - 1;
+		if (currentOriginalNeg < 0) {
+			currentOriginalNeg = originals.length - 1;
+		}
+		images.style.setProperty("pointer-events", "none");
+		currentCard -= dif;
+
+		images.children[currentCard + dif].classList.remove(styles.visible);
+		images.children[currentCard].classList.add(styles.visible);
+
+		for (var i = 0; i < dif; i++) {
+			const clone = originals[currentOriginalNeg].cloneNode(true);
+			images.insertBefore(clone, images.children[0]);
+			currentOriginalNeg += 1;
+			if (currentOriginalNeg > originals.length) {
+				currentOriginalNeg = currentOriginalNeg - originals.length;
+			}
+		}
+		images.style.transitionDuration = "0s";
+		images.style.transform = `translate(-${(startCard + dif) * cardWidth - mobileDif}px)`;
+
+		setTimeout(() => {
+			images.style.transitionDuration = "0.5s";
+			images.style.transform = `translate(-${startCard * cardWidth - mobileDif}px)`;
+		}, 10);
+
+		setTimeout(() => {
+			images.style.setProperty("pointer-events", "all");
+			currentCard += dif;
+			for (var i = 0; i < dif; i++) {
+				images.children[images.children.length - 1].remove();
+			}
+		}, 500);
+	};
 
 	useEffect(() => {
 		if (onScreen) ref.current.classList.add(styles.visible);
@@ -48,90 +132,6 @@ export default function SectionProyects() {
 			});
 		};
 		images.addEventListener("mousedown", onMouseDown);
-
-		const handleCardClickPos = (dif) => {
-			console.log("click pos", dif);
-			const check = images.querySelectorAll(`.${styles.card}`)[
-				images.querySelectorAll(`.${styles.card}`).length - 1
-			];
-			let currentOriginalPos =
-				originals.findIndex(
-					(original) =>
-						original.querySelector(`.${styles["card-title"]}`).innerHTML ===
-						check.querySelector(`.${styles["card-title"]}`).innerHTML
-				) + 1;
-			if (currentOriginalPos > originals.length - 1) {
-				currentOriginalPos = currentOriginalPos - originals.length;
-			}
-			let currentCard = startCard;
-			images.style.setProperty("pointer-events", "none");
-			currentCard += dif;
-			images.style.transitionDuration = "0.5s";
-			images.style.transform = `translate(-${currentCard * cardWidth - mobileDif}px)`;
-			for (var i = 0; i < dif; i++) {
-				const clone = originals[currentOriginalPos].cloneNode(true);
-				images.appendChild(clone);
-				currentOriginalPos += 1;
-				if (currentOriginalPos > originals.length - 1) {
-					currentOriginalPos = currentOriginalPos - originals.length;
-				}
-			}
-			images.children[currentCard].classList.add(styles.visible);
-			images.children[currentCard - dif].classList.remove(styles.visible);
-
-			setTimeout(() => {
-				images.style.setProperty("pointer-events", "all");
-				currentCard -= dif;
-				for (var i = 0; i < dif; i++) {
-					images.children[0].remove();
-				}
-				images.style.transitionDuration = "0s";
-				images.style.transform = `translate(-${currentCard * cardWidth - mobileDif}px)`;
-			}, 500);
-		};
-
-		const handleCardClickNeg = (dif) => {
-			let currentCard = startCard;
-			const check = images.querySelectorAll(`.${styles.card}`)[0];
-			let currentOriginalNeg =
-				originals.findIndex(
-					(original) =>
-						original.querySelector(`.${styles["card-title"]}`).innerHTML ===
-						check.querySelector(`.${styles["card-title"]}`).innerHTML
-				) - 1;
-			if (currentOriginalNeg < 0) {
-				currentOriginalNeg = originals.length - 1;
-			}
-			images.style.setProperty("pointer-events", "none");
-			currentCard -= dif;
-
-			images.children[currentCard + dif].classList.remove(styles.visible);
-			images.children[currentCard].classList.add(styles.visible);
-
-			for (var i = 0; i < dif; i++) {
-				const clone = originals[currentOriginalNeg].cloneNode(true);
-				images.insertBefore(clone, images.children[0]);
-				currentOriginalNeg += 1;
-				if (currentOriginalNeg > originals.length) {
-					currentOriginalNeg = currentOriginalNeg - originals.length;
-				}
-			}
-			images.style.transitionDuration = "0s";
-			images.style.transform = `translate(-${(3 + dif) * cardWidth - mobileDif}px)`;
-
-			setTimeout(() => {
-				images.style.transitionDuration = "0.5s";
-				images.style.transform = `translate(-${3 * cardWidth - mobileDif}px)`;
-			}, 10);
-
-			setTimeout(() => {
-				images.style.setProperty("pointer-events", "all");
-				currentCard += dif;
-				for (var i = 0; i < dif; i++) {
-					images.children[images.children.length - 1].remove();
-				}
-			}, 500);
-		};
 
 		const onResize = (e) => {
 			let cw;
@@ -213,6 +213,21 @@ export default function SectionProyects() {
 		images.appendChild(firstCardClone);
 	}, [images]);
 
+	const handlers = useSwipeable({
+		onSwipedLeft: (eventData) => handleCardClickPos(1),
+		onSwipedRight: (eventData) => handleCardClickNeg(1),
+	});
+
+	const cardsRef = useRef(null);
+
+	useEffect(() => {
+		if (!cardsRef.current) return;
+		const cards = cardsRef.current.children;
+		const startCard = Math.floor((cards.length + 1) / 2);
+		setStartCard(startCard);
+		cards[startCard - 1].classList.add(styles.visible);
+	}, [cardsRef]);
+
 	return (
 		<section id="proyects" className={styles.proyects} ref={ref}>
 			<h2 className="spanText">
@@ -227,10 +242,9 @@ export default function SectionProyects() {
 				<span>s</span>
 			</h2>
 
-			<div id="carrousel" className={styles.carrousel}>
-				<div className={styles.cards} style={{ transform: "translate(-920px)" }}>
+			<div id="carrousel" className={styles.carrousel} {...handlers}>
+				<div ref={cardsRef} className={styles.cards} style={{ transform: "translate(-920px)" }}>
 					<Project
-						visible={startCard === 1}
 						title="Kubikware"
 						description="Recreación de la página de kubikware."
 						stacks={["HTML", "CSS", "JS"]}
@@ -238,7 +252,6 @@ export default function SectionProyects() {
 						image={"/img/kubikware-remake.png"}
 					/>
 					<Project
-						visible={startCard === 2}
 						title="RL Stats"
 						description="Una aplicación móvil que da acceso a cualquier persona a sus rangos de Rocket League
 						desde su teléfono. Esta aplicación está pensada para todos los jugadores de Rocket
@@ -250,7 +263,6 @@ export default function SectionProyects() {
 						image={"/img/rocket stats.png"}
 					/>
 					<Project
-						visible={startCard === 3}
 						title="Satellites On Fire"
 						description="Satellites On Fire es un sistema de detección en tiempo real de focos de calor, los cuales contemplan distintos niveles de probabilidad de ser incendios. El usuario puede observarlos en tiempo real, teniendo también la posibilidad de ver focos pasados. Adicionalmente, brindamos un sistema de alertas para que los usuarios puedan recibir notificaciones de los últimos focos detectados en sus zonas, permitiendoles actuar a tiempo, reduciendo costos causados por la propagación de los incendios."
 						stacks={["Front-end dev", "React", "Material-UI"]}
@@ -258,7 +270,13 @@ export default function SectionProyects() {
 						image={"/img/sof.png"}
 					/>
 					<Project
-						visible={startCard === 4}
+						title="Zerti"
+						description="Zerti es un proyecto Web3 cuyo objetivo central es asegurar la validez y confianza de las certificaciones, aptitudes y títulos emitidos por las instituciones académicas, organizaciones y empresas, basándose en la inmutabilidad y transparencia de la tecnología blockchain."
+						stacks={["React", "Web3", "NodeJs"]}
+						link="https://zerti.com.ar/"
+						image={"/img/zerti.png"}
+					/>
+					<Project
 						title="InFocus"
 						description="Una página web que brinda herramientas de ayuda a jóvenes y adultos en etapa laboral
 							con déficit de atención. Se brindan herramientas tales como recordatorios,
@@ -272,7 +290,6 @@ export default function SectionProyects() {
 						image={"/img/infocus.png"}
 					/>
 					<Project
-						visible={startCard === 5}
 						title="Calcular Fechas"
 						description="Una aplicación móvil para ver tus eventos de una forma más divertida. Podrás crear 2
 							tipos de eventos, pasados o futuros. Para los eventos futuros, podrás ponerle un
@@ -288,7 +305,6 @@ export default function SectionProyects() {
 						image={"/img/calcular-fechas.png"}
 					/>
 					<Project
-						visible={startCard === 6}
 						title="Wordle Infinito"
 						description="Clásico juego de Wordle pero sacando la necesidad de esperar 24 horas para poder jugar con otra palabra."
 						stacks={["HTML", "CSS", "JS"]}
