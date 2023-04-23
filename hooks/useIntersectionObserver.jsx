fc7f0f46940edf3b41b8fcd64cd2870aeb5b0524
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2023-04-23 12:30:03 -0300

    About me anim only on enter

--- hooks/useIntersectionObserver.jsx
+++ hooks/useIntersectionObserver.jsx
@@ -4,10 +4,12 @@ export default function useIntersectionObserver(ref, options) {
 	const [isIntersecting, setIsIntersecting] = React.useState(false);
 
 	React.useEffect(() => {
-		if(!ref?.current) return;
+		if (!ref?.current) return;
 		const current = ref.current;
 		const observer = new IntersectionObserver(([entry]) => {
+			if (isIntersecting) return;
 			setIsIntersecting(entry.isIntersecting);
+			if (entry.isIntersecting) observer.unobserve(current);
 		}, options);
 
 		if (current) {
