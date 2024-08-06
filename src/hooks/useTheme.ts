0c4c59a998f7d71e8a131d445850971b5c4f646a
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2024-08-06 15:59:37 -0300

    feat: save theme and use systems as default

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,10 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 const useTheme = () => {
-	const [theme, setTheme] = useState<string | null>("dark");
+	const [theme, setTheme] = useState<string | null>(null);
+
 	useEffect(() => {
 		const observer = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
@@ -14,14 +17,32 @@ const useTheme = () => {
 
 		observer.observe(document.documentElement, { attributes: true });
 
+		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+			const theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+			document.documentElement.setAttribute("theme", theme);
+		});
+
 		return () => {
 			observer.disconnect();
 		};
 	}, []);
 
+	useEffect(() => {
+		const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light";
+
+		const localTheme = localStorage.getItem("theme");
+
+		if (!localTheme) document.documentElement.setAttribute("theme", systemTheme);
+		else document.documentElement.setAttribute("theme", localTheme);
+	}, [theme]);
+
 	const switchTheme = () => {
 		const theme = document.documentElement.getAttribute("theme");
-		document.documentElement.setAttribute("theme", theme === "dark" ? "light" : "dark");
+		const newTheme = theme === "dark" ? "light" : "dark";
+		document.documentElement.setAttribute("theme", newTheme);
+		localStorage.setItem("theme", newTheme);
 	};
 
 	const handleThemeChange = () => {
@@ -29,7 +50,7 @@ const useTheme = () => {
 		if (!document.startViewTransition) switchTheme();
 		// @ts-ignore
 		else document.startViewTransition(switchTheme);
-	}
+	};
 
 	return { theme, switchTheme: handleThemeChange };
 };
